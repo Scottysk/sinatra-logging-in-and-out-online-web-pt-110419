dcbba51f8cbf40355087e46e396afb968a3066f2
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2020-03-03 03:06:49 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,10 @@
 
 ActiveRecord::Schema.define(version: 0) do
   
-  
+   create_table "users", force: :cascade do |t|
+    t.string  "username"
+    t.string  "password"
+    t.decimal "balance"
+  end
 
 end
